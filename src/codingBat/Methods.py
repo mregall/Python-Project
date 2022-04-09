04414d3245e6600ddabf2a491d0d22c058b84326
mregall <michael.regall@gmail.com>
2022-04-09 09:38:35 -0500

    A few more done

--- src/codingBat/Methods.py
+++ src/codingBat/Methods.py
@@ -4,3 +4,35 @@ def array123(nums):
         if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3:
             return True
     return False
+
+
+def monkey_trouble(a_smile, b_smile):
+    if a_smile and b_smile:
+        return True
+    if not a_smile and not b_smile:
+        return True
+    return False
+
+# return ((a_smile and b_smile) or (not a_smile and not b_smile))
+
+# return (a_smile == b_smile)
+
+
+def sum_double(a, b):
+    if a != b:
+        return a + b
+    if a == b:
+        return (a + b) * 2
+
+
+def diff21(n):
+    if n <= 21:
+        return 21 - n
+    else:
+        return (n - 21) * 2
+
+
+def parrot_trouble(talking, hour):
+    return talking and (hour < 7 or hour > 20)
+
+
